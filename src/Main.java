fb749c9e0d324c65042376fbf694333b6b9d1ca4
vishnushaga <vishnushagareddy@gmail.com>
2022-06-20 23:24:20 -0500

    second commit added while loop.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,16 @@ public class Main {
 
         for (int i = 1; i <= 5; i++)
             System.out.println("Hello World");
+
+        int i = 1;
+
+        // test expression
+        while (i < 6) {
+            System.out.println("Hello World");
+
+            // update expression
+            i++;
+        }
         }
     }
 
